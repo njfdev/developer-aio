e6536172c6bcee323f7d0f7edb4d4116cf985b31
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2023-01-05 19:55:31 -0500

    feat: create global.css and compile all css files

--- gulpfile.js
+++ gulpfile.js
@@ -35,7 +35,7 @@ function hbs(done) {
 function css(done) {
     pump(
         [
-            src("assets/css/screen.css", { sourcemaps: true }),
+            src("assets/css/*.css", { sourcemaps: true }),
             postcss([easyimport, autoprefixer(), cssnano()]),
             dest("assets/built", { sourcemaps: "." }),
             livereload(),
